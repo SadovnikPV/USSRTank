4c3765c7165d95e1bda648722910fffadac9d31e
SadovnikPV <80878723+SadovnikPV@users.noreply.github.com>
2021-03-25 04:58:41 +0300

    v1.0

---
+++ index.php
@@ -0,0 +1,55 @@
+﻿<!DOCTYPE html>
+<head>
+	<title> Танчики </title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+	<meta name="description" content="" />
+	<meta http-equiv="Content-Language" content="ru">
+	<meta name="robots" content="index, follow" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	
+<div id = "page_wrap">
+	<div id = "header">
+		<p> <a href = "javascript:void(0)" onclick = "close_logo()"> Скрыть логотип </a> </p>
+	</div>
+	<div id = "header_mini">
+		<p> <a href = "javascript:void(0)" onclick = "show_logo()"> Показать логотип </a> </p>
+	</div>
+
+	
+	<div id = "main_content">
+
+		<div id = "tip">
+			Подсказка: для удобства игры, вы можете закрыть логотип сайта, нажав на кнопку "Скрыть логотип" в его правом нижнем углу, а также уменьшить или увеличить масштаб экрана, зажав на клавиатуре клавишу ctrl и прокрутив колесико мышки.
+			<a href = "javascript:void(0)" onclick = "close_tip()"> Закрыть подсказку </a>
+		</div>
+
+		<canvas id="canvas"></canvas>
+		<script type="text/javascript" src="js/jquery3.5.1.js"></script>
+		<script type="text/javascript" src="js/jqueryrotate.2.1.js"></script>
+		<script type="text/javascript" src="js/globalSettings.js"></script>
+		<script type="text/javascript" src="js/otherFunction.js"></script>
+		<script type="text/javascript" src="js/eventDetector.js"></script>
+		<script type="text/javascript" src="js/levels.js"></script>
+		<script type="text/javascript" src="js/objAll.js"></script>
+		<script type="text/javascript" src="js/objBase.js"></script>
+		<script type="text/javascript" src="js/objWall.js"></script>
+		<script type="text/javascript" src="js/objMonolith.js"></script>
+		<script type="text/javascript" src="js/objWater.js"></script>
+		<script type="text/javascript" src="js/objBrick.js"></script>
+		<script type="text/javascript" src="js/objWood.js"></script>
+		<script type="text/javascript" src="js/objBridgeCover.js"></script>
+		<script type="text/javascript" src="js/objBullet.js"></script>
+		<script type="text/javascript" src="js/objTank.js"></script>
+		<script type="text/javascript" src="js/gameMap.js"></script>
+		<script type="text/javascript" src="js/game.js"></script>
+	</div>
+	
+	<div id = "footer">
+		USSR Tank 2021
+	</div>
+</div>
+</body>
+</html>
+
---
+++ level_constructor.php
@@ -0,0 +1,105 @@
+﻿<!DOCTYPE html>
+<head>
+	<title> Танчики. Конструктор уровней </title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+	<meta name="description" content="" />
+	<meta http-equiv="Content-Language" content="ru">
+	<meta name="robots" content="index, follow" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id = "ConstructorLevelDescription">
+		Название уровня:
+		<input class = "ConstructorInputText" id = "levelTitle" type="text"> <br>
+		Описание уровня: <br>
+		<textarea class = "ConstructorInputTextarea" id = "levelDescriptionText" ></textarea> <br>
+		Фоновое изображение (greenGrace, asphalt):
+		<input class = "ConstructorInputText" id = "levelBgImg" type="text"> 
+	</div>
+	<div id = "ConstructorMainWindow">
+		<div id = "ConstructorControl">
+			<div class = "ConstructorControlButton" onclick = "createNewLevel()">
+				Создать новый уровень
+			</div>
+			<div class = "ConstructorControlButton" onclick = "getLevelCode()">
+				Получить код уровня
+			</div>
+		</div>
+		<div id = "ConstructorLevelMap">
+		</div>
+		<div id = "ConstructorMenu">
+			<div class = "ConstructorMenuCell" onclick = "selectObj('water')">
+				<img class = "ConstructorMenuImg" src = "img/water.png"> Вода
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('monolith_full')">
+				<img class = "ConstructorMenuImg" src = "img/monolith_full.png"> Бетон (полный)
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('monolith')">
+				<img class = "ConstructorMenuImgQuarter" src = "img/monolith_quarter.png"> Бетон (четверть)
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('base')">
+				<img class = "ConstructorMenuImg" src = "img/base.png"> База
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('mapWall')">
+				<img class = "ConstructorMenuImg" src = "img/mapWall.png"> Граница уровня
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('brick_full')">
+				<img class = "ConstructorMenuImg" src = "img/brick_full.png"> Кирпич (полный)
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('brick_quarter')">
+				<img class = "ConstructorMenuImgQuarter" src = "img/brick_quarter.png"> Кирпич (четверть)
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('wood_horizontal')">
+				<img class = "ConstructorMenuImgHalfHorizontal" src = "img/wood_horizontal.png"> Дерево (горизонтально)
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('wood_vertical')">
+				<img class = "ConstructorMenuImgHalfVertical" src = "img/wood_vertical.png"> Дерево (вертикально)
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('tree')">
+				<img class = "ConstructorMenuImg" src = "img/tree.png"> Дерево
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('bridgeCover')">
+				<img class = "ConstructorMenuImg" src = "img/bridgeCover.png"> Брусчатка
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('clearCell')">
+				<img class = "ConstructorMenuImg" src = "img/clearCell.png"> Очистить ячейку
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('separateCell')">
+				<img class = "ConstructorMenuImg" src = "img/separateCell.png"> Разделить ячейку
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "selectObj('uniteCell')">
+				<img class = "ConstructorMenuImg" src = "img/clearCell.png"> Объединить ячейки
+			</div>
+		</div>
+	</div>
+	<div id = "ConstructorTankMainWindow">
+		<div id = "ConstructorTankMenu">
+			<div class = "ConstructorMenuCell" onclick = "addTank('enemyTankBase')">
+				<img class = "ConstructorMenuImg" src = "img/enemyTankBase_up.png"> Легкий танк
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "addTank('enemyTankSpeed')">
+				<img class = "ConstructorMenuImg" src = "img/enemyTankSpeed_up.png"> БТР
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "addTank('enemyTankArmor')">
+				<img class = "ConstructorMenuImg" src = "img/enemyTankArmor_up.png"> Бронированный танк
+			</div>
+			<div class = "ConstructorMenuCell" onclick = "addTank('enemyTankHeavy')">
+				<img class = "ConstructorMenuImg" src = "img/enemyTankHeavy_up.png"> Тяжелый танк
+			</div>
+		</div>
+		<div id = "ConstructorTankAll">
+		</div>
+	</div>
+	
+	<div id = "ConstructorLevelCode">
+	fgfgfg
+	</div>
+
+	<script type="text/javascript" src="js/jquery3.5.1.js"></script>
+	<script type="text/javascript" src="js/jqueryrotate.2.1.js"></script>
+	<script type="text/javascript" src="js/levelConstructor.js"></script>
+
+
+</body>
+</html>
+
